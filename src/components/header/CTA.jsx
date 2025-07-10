5f17828c1a6f240cb9ee3a452642f2f9df24396e
sarang salame <115799065+sarangsalame@users.noreply.github.com>
2025-07-10 09:44:57 +0530

    change opening path of file

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,7 +3,7 @@ import CV from '../../assets/sarangDeveloper.pdf'
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={CV} download className='btn'>Download CV</a>
+        <a href={CV} target='_blank' download className='btn'>Download CV</a>
         <a href='#contact' className='btn btn-primary'>Let's Talk</a>
     </div>
   )
