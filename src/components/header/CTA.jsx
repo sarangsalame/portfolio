cfd4876a0bd091814e7dec6e8cebeffee543437b
sarang salame <115799065+sarangsalame@users.noreply.github.com>
2025-11-28 13:45:48 +0530

    Update CV file reference in CTA component

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import CV from '../../assets/sarangDeveloper.pdf'
+import CV from '../../assets/Resume_SarangSalame.pdf'
 const CTA = () => {
   return (
     <div className='cta'>
